dcc4525f77705782651348280850332d6fc18a48
dawei shen <shendawei0727@163.com>
2013-01-09 01:46:24 +0800

    nothing

--- project/src/com/code/subdemo/Color_Drawable_Diff.java
+++ project/src/com/code/subdemo/Color_Drawable_Diff.java
@@ -5,7 +5,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * 两个ImageView分别以color、drawable的方式设置resource，证明两个方式的区别
+ * 在Resource.xml 中定义color颜色 和 drawable颜色。
+ * 两个ImageView/ImageButton分别以color、drawable的方式设置resource，证明两个方式的区别
+ * 
+ * 结论：从效果图上看，没有任何区别。
  */
 public class Color_Drawable_Diff extends Activity
 {
